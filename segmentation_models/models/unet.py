c0f7479fb8dc0de997b1285fd902e523bc136473
JeremyOng96 <jeremyunderscore@gmail.com>
2020-11-22 20:37:49 +0800

    Update unet.py

--- segmentation_models/models/unet.py
+++ segmentation_models/models/unet.py
@@ -67,10 +67,7 @@ def DecoderUpsamplingX2Block(filters, stage, use_batchnorm=False):
 
     return wrapper
 
-def DecoderUpsamplingX2BlockSA(filters, stage, use_batchnorm=False, Rk=1, Rv =1, Nh = 8):
-    ei = lambda x : int(np.ceil(x/Nh)*Nh)
-    dk = ei(filters*Rk)
-    dv = ei(filters*Rv)
+def DecoderUpsamplingX2BlockSA(filters, stage, use_batchnorm=False):
     up_name = 'decoder_stage{}_upsampling'.format(stage)
     conv1_name = 'decoder_stage{}a'.format(stage)
     conv2_name = 'decoder_stage{}b'.format(stage)
