a711df0bb80ed3fe9e2d421debb1946fc8664716
JeremyOng96 <jeremyunderscore@gmail.com>
2020-11-24 21:06:31 +0800

    Update unet.py

--- segmentation_models/models/unet.py
+++ segmentation_models/models/unet.py
@@ -144,7 +144,7 @@ def build_unet(
 ):
     input_ = backbone.input
     x = backbone.output
-    x = MultiHeadAttention(512,name='output')(x)
+    x = MultiHeadAttention(512,stage='output')(x)
     # extract skip connections
     skips = ([backbone.get_layer(name=i).output if isinstance(i, str)
               else backbone.get_layer(index=i).output for i in skip_connection_layers])
