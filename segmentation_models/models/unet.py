1911a61582ca14f662fb3b61af85c33393595e2b
JeremyOng96 <jeremyunderscore@gmail.com>
2020-12-14 21:49:12 +0800

    Update unet.py

--- segmentation_models/models/unet.py
+++ segmentation_models/models/unet.py
@@ -103,7 +103,7 @@ def DecoderUpsamplingX2BlockCBAM(filters, stage, use_batchnorm=False):
             # This layer is used to reduce the semantic difference between encoder and decoder features before concatenation
             # Adds attention to the encoder features
             
-            skip = GCN(out_c=filters,k)(skip)
+            skip = GCN(out_c=filters,k=k)(skip)
             skip = BR(out_c=filters)(skip)
             skip = cbam_block()(skip)
             x = layers.Concatenate(axis=concat_axis, name=concat_name)([x, skip])
